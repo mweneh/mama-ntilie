0d2a255fd900df82edfc3286ba69fa776015e86d
David <davidongaroh@gmail.com>
2023-04-03 21:51:16 +0300

    add custom messages/response

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -11,4 +11,11 @@ class AuthenticationController < ApplicationController
     def auth_params
     params.permit(:username, :email, :pass)
     end
+    def account_created
+        app_response(status: :created, message: "Account created")
+    end
+      
+    def account_creation_failed(errors)
+    app_response(status: :unprocessable_entity, message: "Account creation failed", body: errors)
+    end
 end
