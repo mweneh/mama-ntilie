8cd87b7b7d601cf00a4e9c06ae98d1c595412502
David <davidongaroh@gmail.com>
2023-04-03 22:13:27 +0300

    use the new code in login & signup

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,17 +1,18 @@
 class AuthenticationController < ApplicationController
     def login_user
-        user = User.where(["username = :username OR email = :email", {username: auth_params[:username], email: auth_params[:email]}] )
-        user.first&.authenticate(auth_params[:pass])? account_login : account_login_failed
+        user = User.where(["username = :username OR email = :email", 
+                           {username: auth_params[:username], email: auth_params[:email]}] )
+        user.first&.authenticate(auth_params[:pass])? account_login : account_login(failed: true)
     end
-    
+      
     def register_user
         user = User.create(username: auth_params[:username], email: auth_params[:email], password: auth_params[:pass])
-        user.valid? ? account_created : account_creation_failed(user.errors)
+        user.valid? ? account_created : account_created(failed: true, errors: user.errors)
     end
 
     private
     def auth_params
-    params.permit(:username, :email, :pass)
+        params.permit(:username, :email, :pass)
     end
     def account_created(failed: false, errors: nil)
         app_response(status: failed ? :unprocessable_entity : :created,
