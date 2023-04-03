41e4595b79e81791fe05b602a603b2e6375da326
David <davidongaroh@gmail.com>
2023-04-03 21:46:50 +0300

    add response method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    def app_response(status: 200, message: nil, body: nil)
+        render json: {
+           status: status,
+           message: message,
+           body: body
+       }, status: status
+      end
 end
