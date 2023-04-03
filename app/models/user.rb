fcd51d6a96e31d6dd226781a396eede1275ef1c7
David <davidongaroh@gmail.com>
2023-04-03 08:19:28 +0300

    add inbuilt validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :recipes
   enum :u_type, {user: 0, admin: 1}
   has_secure_password
+  validates :email, { presence: true, uniqueness: true }
+  validates :username, {presence: true, uniqueness: true, length: { minimum: 6 } }
 end
