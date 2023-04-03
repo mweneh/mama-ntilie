f298f3d473a7f44ddb4c4d50de76a06384ca50d7
David <davidongaroh@gmail.com>
2023-04-03 08:35:10 +0300

    refactor auth requests

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "Authentications", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+RSpec.describe AuthenticationController, type: :controller do
+ 
 end
