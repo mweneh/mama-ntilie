d638cff75db173e0293d5a0c23084d5fd69994f5
David <davidongaroh@gmail.com>
2023-04-06 14:44:22 +0300

    add JWT decode/encode methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+
+    # jwt secret key
+    SECRET_KEY = ENV['JWT_SECRET']
+
     def app_response(status: 200, message: nil, body: nil)
         render json: {
            status: status,
@@ -9,5 +13,19 @@ class ApplicationController < ActionController::API
     
     def not_found(message)
     app_response(status: 404, message: message)
+    end
+
+        # JWT Encoding
+    def encode_data(payload)
+        JWT.encode(payload, SECRET_KEY, 'HS256')
+    end
+
+    # JWT Decoding
+    def decode_data(token)
+        begin
+        JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })
+        rescue JWT::DecodeError => e
+        nil
+        end
     end
 end
